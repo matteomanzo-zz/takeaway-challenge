8c0730a0880df289a176ca82030af6de8772814c
matteomanzo <imatteo.manzo@gmail.com>
2014-12-21 19:49:26 +0000

    dishes should have prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,9 +1,12 @@
 class Menu
-  
+
     attr_reader :dishes
 
   def initialize
-    @dishes = [{item: 'chicken', price: 10}]
+    @dishes = [{item: 'chicken caesar salad', price: 10},
+               {item: 'pizza', price: 12},
+               {item: 'hamburger', price: 15},
+               {item: 'spaghetti bolognese', price: 12}]
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,8 +4,14 @@ describe Menu do
 
   let(:menu) {Menu.new}
 
-  it 'should contain dishes' do
-    expect(menu.dishes)
-  end
+    it 'should contain dishes' do
+      expect(menu.dishes)
+    end
 
+  context 'dishes' do
+
+    it 'should have prices' do
+      expect(menu.dishes[0][:price]).to match(10)
+    end
+  end
 end
