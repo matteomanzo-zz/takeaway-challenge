e64a92fef62d5dae9f5c78b4ceb31c30952176f7
matteomanzo <imatteo.manzo@gmail.com>
2014-12-30 12:41:26 +0000

    created .gitignore file for account_sid and auth_token

--- takeaway.rb
+++ takeaway.rb
@@ -2,10 +2,13 @@ require './lib/shop.rb'
 require 'rubygems'
 require 'twilio-ruby'
 require 'colorize'
+require './ACCOUNT_SID.rb'
+require './AUTH_TOKEN.rb'
 
 @lineWidth = 50
 
 @shop = Shop.new
+@client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 @view_dishes = []
 @shop.menu.dishes.each {|dish, price| @view_dishes << ("#{dish[:name]} - £#{dish[:price]}".blue.center(@lineWidth+10))}
 
@@ -37,7 +40,8 @@ def select options
   when '3' then !@shop.customer.basket.empty? ? (puts @shop.customer.basket) : (puts 'Your basket is empty!')
   when '4' then @shop.customer.remove_dish(gets.chomp)
   when '5' then @shop.customer.basket.clear
-  when '6' then 
+  when '6' then @shop.confirm
+    send_text! if !@shop.order.empty?
   when '0' then exit
   end
 end
@@ -48,6 +52,17 @@ def process
     select(STDIN.gets.chomp)
   end
 end
+
+def send_text!
+    one_hour = 60*60
+    time = Time.now + one_hour
+    @client.account.sms.messages.create(
+      :from => "+441524220092",
+      :to =>   "+447479198894",
+      :body => "Thank you! Your order was placed and will be delivered before #{time}"
+    )
+  end
+
 
 intro
 process
